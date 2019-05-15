03affec59bd439c6434711c074a3020885ab2297
Blair Brownlie <bbrownli@asu.edu>
2019-05-14 23:51:04 -0700

    making sure I can serve my html pages before I try to modularize them completely

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const port = process.env.port || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, 'home.html'))
+});
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
 app.listen(port, function (){
     console.log('successfully connected on localhost:'+port);
 })
