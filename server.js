831c5ed1d31afe0258790c7df3d6ffd7421bf45c
Blair Brownlie <bbrownli@asu.edu>
2019-05-15 17:12:27 -0700

    still trying to get heroku to serve what little of the html I have

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 let app = express();
 
-const port = process.env.port;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -15,6 +15,6 @@ app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
-app.listen(port, function (){
-    console.log('successfully connected on localhost:'+port);
+app.listen(PORT, function (){
+    console.log('successfully connected on'+ PORT);
 });
